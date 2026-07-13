0c7ed4416966405f5186ed98481c4d2d4bbff66c
Tarde Cetafaju <anna.l.costa6@aluno.senai.br>
2026-07-13 16:42:13 -0300

    alterado

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,21 +1,26 @@
 //CRIANDO O ARRAY DE ITENS DO CARRINH0
-const itensCarrinho = JSON.parse(localStorage.get('carrinhoSessao')) || []
-const itensCarrinho2 = JSON.parse(sessionStorage.get('carrinhoSessao')) || []
+const itensCarrinho = JSON.parse(sessionStorage.getItem("carrinhoSessao")) || []
+//const itensCarrinho2 = JSON.parse(sessionStorage.get('carrinhoSessao')) || []
 
 //FUNÇÃO PARA ADICIONAR UM ITEM
 const addItem = (objItem) => {
     itensCarrinho.push(objItem)
 
+    sessionStorage.setItem('carrinhoSessao', itensCarrinho)
     listItens()
 }
 
 //FUNÇÃO PARA LISTAR OS ITENS DO CARRINHO
 const listItens = () => {
-    itensCarrinho.forEach((elem, i) => {
-        console.log(`elemento ${i + 1} - ${elem}`)
-    })
+    const listaItens = JSON.parse(sessionStorage.getItem('carrinhoSessao'))
 
-}
+    itensCarrinho.forEach((elem, i) => {
+        //console.log(`elemento ${i + 1} - ${elem. descricao_produto} - ${elem.valor_unitario}${elem.unidade}`)
+        alert(`elemento ${i + 1} - ${elem. descricao_produto} - ${elem.valor_unitario}${elem.unidade}`)
+    })
+    }
+
+
 
 //EXPORTAÇÃO
 export{addItem}
