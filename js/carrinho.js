097d7a2fa3e93f4777102ac2b7b9705c6c59c919
Tarde Cetafaju <anna.l.costa6@aluno.senai.br>
2026-07-14 13:35:47 -0300

    alterado

--- js/carrinho.js
+++ js/carrinho.js
@@ -40,8 +40,38 @@ const montaTelaCarrinho = (objListaItens) => {
 
         const divDescricao = document.createElement('div')
         divDescricao.setAttribute('class', 'descricao')
-        
+        divDescricao.innerHTML = elem.descricao_produto
 
+        const divValores = document.createElement('div')
+        divValores.setAttribute('class', 'valores')
+
+        const pItem = document.createElement('p')
+        pItem.innerHTML = `R$ ${parseFloat(elem. valor_unitario).toFixed
+        (2).replace('.', '.')}`
+
+        const divQuant = document.createElement('div')
+        divQuant.setAttribute('class', input-quantidade)
+
+        const inputQuantidade = document.createElement('input')
+        inputQuantidade.setAttribute('type', 'number')
+        inputQuantidade.setAttribute('name', `quant${i}`)
+        inputQuantidade.setAttribute('id', `quant${i}`)
+        inputQuantidade.setAttribute('class', 'input-item')
+        inputQuantidade.setAttribute('value', 1)
+
+        divQuant.appendChild(inputQuantidade)
+
+        const pCal = document.createElement('p')
+        pCalc.innerHTML = `R$ ${elem.valor_unitario * 1}`
+
+        const imgRemover = document.createElement('img')
+        imgRemover.setAttribute('src', '../imagens/icones/remover.png')
+        imgRemover.setAttribute('alt', 'remover')
+
+        divValores.appendChild(pItem)
+        divValores.appendChild(divQuant)
+        divValores.appendChild(pCalc)
+        divValores.appendChild(imgRemover)
 
 
     })
