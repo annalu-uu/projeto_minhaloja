890b7aee4590d0fd4f6004adde1118e62fc3b479
Tarde Cetafaju <anna.l.costa6@aluno.senai.br>
2026-07-17 16:40:56 -0300

    criada a pasta   js/consulta_cep.js e alterado

---
+++ js/consulta_cep.js
@@ -0,0 +1,52 @@
+//PEGANDO ELEMENTO DO DOM
+const inputCep = document.querySelector('#cep')
+
+//CAPTURANDO O EVENTO change
+inputCep.addEventListener('change',(evt) => {
+
+    const numCep = evt.target.value.replace(/\D/g, "")
+
+    if (numCep.length !== 8){
+        alert('CEP INVÁLIDO')
+
+        return
+    }
+
+    consultaCep(numCep)
+
+
+})
+
+//FUNÇÃO CONSULTA CEP VIACEP
+const consultaCep = async (cep) => {
+    //TENTA CONECTAR A API
+    try{
+        //FAZ A COMUNICAÇÃO COM A API DO VIA CEP POR MEIO DA FUNÇÃO fetch
+        //AWAIT - AGUARDA ATÉ OBTER UM PROMISE
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+
+    //SE O STATUS DA RESPOSTA NÃO FOR OK. DISPARA UMA EXCEÇÃO    
+    if(!resposta.ok){
+        throw new Error("ERRO NA REQUISIÇÃO")
+    }
+
+    //OBTEM OS DADOS DA API
+    const dadosEndereco = await resposta.json()
+
+    //VERIFICA SE OS DADOS SÃO VÁLIDOS
+    if(dadosEndereco.erro){
+        alert('CEP NÃO LOCALIZADO')
+
+        return
+    }
+
+    //CHAMA A FUNÇÃO carregaInput
+    carregaInput(dadosEndereco)
+
+//CASO HAJA QUALQUER ERRO É DISPARADA UMA EXCEÇÃO
+}catch(erro){
+    console.log("ERRO", erro.message)
+}
+
+
+}
