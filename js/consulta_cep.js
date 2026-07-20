2ce0ba7624d04f4428c2e32889b5622d0b011e8e
Tarde Cetafaju <anna.l.costa6@aluno.senai.br>
2026-07-20 13:30:09 -0300

    alterado

--- js/consulta_cep.js
+++ js/consulta_cep.js
@@ -2,63 +2,71 @@
 const inputCep = document.querySelector('#cep')
 
 //CAPTURANDO O EVENTO change
-inputCep.addEventListener('change',(evt) => {
-
+inputCep.addEventListener('change', (evt) => {
+    //PEGANDO OS NÚMEROS DOS INPUTCEP
     const numCep = evt.target.value.replace(/\D/g, "")
-
-    if (numCep.length !== 8){
+    //VERIFICA SE POSSUI 8 DÍGITOS
+    if (numCep.length !== 8) {
         alert('CEP INVÁLIDO')
 
         return
     }
-
-    consultaCep(numCep)
-
+    //CHAMA A FUNÇÃO consultaCEP
+    consultaCEP(numCep)
 
 })
 
 //FUNÇÃO CONSULTA CEP VIACEP
-const consultaCep = async (cep) => {
+const consultaCEP = async (cep) => {
     //TENTA CONECTAR A API
     try{
         //FAZ A COMUNICAÇÃO COM A API DO VIA CEP POR MEIO DA FUNÇÃO fetch
         //AWAIT - AGUARDA ATÉ OBTER UM PROMISE
         const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
 
-    //SE O STATUS DA RESPOSTA NÃO FOR OK. DISPARA UMA EXCEÇÃO    
-    if(!resposta.ok){
-        throw new Error("ERRO NA REQUISIÇÃO")
+        //SE O STATUS DA RESPOSTA NÃO FOR OK. DISPARA UMA EXCEÇÃO
+        if(!resposta.ok){
+            throw new Error("ERRO NA REQUISIÇÃO")
+        }
+
+        //OBTEM OS DADOS DA API
+        const dadosEndereco = await resposta.json()
+
+        //VERIFICA SE O DADOS SÃO VÁLIDOS
+        if(dadosEndereco.erro){
+            alert('CEP NÃO LOCALIZADO')
+
+            return
+        }
+
+        //CHAMA A FUNÇÃO carregaInput
+        carregaInput(dadosEndereco)
+
+    //CASO HAJA QUALQUER ERRO É DISPARADA UMA EXCEÇÃO
+    }catch(erro){
+        console.log("ERRO", erro.message)
     }
 
-    //OBTEM OS DADOS DA API
-    const dadosEndereco = await resposta.json()
-
-    //VERIFICA SE OS DADOS SÃO VÁLIDOS
-    if(dadosEndereco.erro){
-        alert('CEP NÃO LOCALIZADO')
-
-        return
-    }
-
-    //CHAMA A FUNÇÃO carregaInput
-    carregaInput(dadosEndereco)
-
-//CASO HAJA QUALQUER ERRO É DISPARADA UMA EXCEÇÃO
-}catch(erro){
-    console.log("ERRO", erro.message)
-    }
-
-
+    
 }
+
 //OBJETO LITERAL DOS INPUTS
 const campos = {
     logradouro: document.querySelector('#logradouro'),
     bairro: document.querySelector('#bairro'),
     localidade: document.querySelector('#localidade'),
     uf: document.querySelector('#uf')
+
 }
 
 //FUNÇÃO CARREGA INPUTS
-const carregaInput = (objEndereco) =>{
+const carregaInput = (objEndereco)=>{
+    const divEndereco = document.querySelector('#div-dados-endereco')
+    divEndereco.classList.remove('oculto')
+
+    for(let campo in objEndereco){
+        campos[campo].value = objEndereco[campo]
+        campos[campo].disabled = true
+    }
 
 }
